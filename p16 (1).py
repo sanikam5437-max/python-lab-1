7955d986e7fe84442babc7662229e724ba5183fd
sanikam5437-max <sanikam5437@gmail.com>
2026-04-26 22:39:32 +0530

    Add files via upload

---
+++ p16 (1).py
@@ -0,0 +1,6 @@
+#Membership operator
+numbers=[10,20,30,40,50]
+#check if 20 is in the list
+print("Is 20 in list?",20 in numbers)
+#check if 60 is not in the list
+print("Is 60 is not in list?",60 not in numbers)
